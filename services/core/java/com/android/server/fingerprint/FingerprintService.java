70f9576c6df891d1755a7a1567ce111ada76d9e8
Razhor <zamorano0695@gmail.com>
2019-01-28 08:09:00 +0000

    Revert "FingerprintService: add property to prevent cleanup of unused fingerprints"
    This reverts commit c0e54be8f759652a19638db2cd4bf2ed8ae36f28.

--- services/core/java/com/android/server/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/fingerprint/FingerprintService.java
@@ -62,7 +62,6 @@ import android.os.RemoteException;
 import android.os.SELinux;
 import android.os.ServiceManager;
 import android.os.SystemClock;
-import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.security.KeyStore;
@@ -339,7 +338,7 @@ public class FingerprintService extends SystemService implements IHwBinder.Death
      * @param userId
      */
     private void doFingerprintCleanupForUser(int userId) {
-        if (SystemProperties.getBoolean("ro.fingerprint.cleanup.unused", CLEANUP_UNUSED_FP)) {
+        if (mCleanupUnusedFingerprints) {
             enumerateUser(userId);
         }
     }
